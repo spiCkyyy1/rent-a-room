bcbf8f284ab3bb5ec4e4ec56c7f9347faec40c48
spiCkyyy1 <hassam_waheed@outlook.com>
2023-02-09 02:20:25 +0000

    Changing welcome text

--- src/main/java/com/example/rentrooms/HelloController.java
+++ src/main/java/com/example/rentrooms/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("Do you want to rent a room?");
     }
 }
